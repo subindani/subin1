00e4433ce2129852b4b5207cccb8ee61e0d1ac5f
SubinDaniVarughese <subin.dani@gmail.com>
2016-08-29 19:23:15 +0000

    just saving

---
+++ com.subin.build.maven.java/src/main/java/com/subin/build/maven/java/App.java
@@ -0,0 +1,20 @@
+package com.subin.build.maven.java;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import io.jsonwebtoken.*;
+import java.util.Date;    
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
